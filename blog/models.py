533c0f9f7e0389c9e17b24719fabec60190b2115
Craig <craigywu@gmail.com>
2023-04-05 14:37:43 +0200

    Added docstrings to blog models

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,15 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 # Default behaviour - Django creates primary keys for you
+    '''
+    Create model with fields title, body, signature and date that represents a blog post
+
+    param: models.Model: Using django built in models module
+
+    return: Title of the blog post
+
+    rtype: string
+    '''
     title = models.CharField(max_length=140)
     body = models.TextField()
     signature = models.CharField(max_length=140, default="Craig")
