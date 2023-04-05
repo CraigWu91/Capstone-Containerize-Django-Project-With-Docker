1b5c48630b87b77c7ad6accc09472438d38892b2
Craig <craigywu@gmail.com>
2023-04-05 14:36:04 +0200

    Added docstrings to blog views

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,13 @@ from django.utils.decorators import method_decorator
 @login_required
 # Create your views here.
 def header(request):
+    '''
+    Renders blog post html template with list of all blog posts
+
+    param: request: http request
+
+    return: http response with blog html content
+    '''
     object_list = Post.objects.all()
     return render(request, "blog/blog.html", {"blog": object_list})
 
