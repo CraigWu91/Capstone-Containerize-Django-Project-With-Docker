73186f4fcb45fe61f69823775a301afa1ba79181
Craig <craigywu@gmail.com>
2023-04-05 14:40:46 +0200

    Added docstrings to personal views

--- personal/views.py
+++ personal/views.py
@@ -5,10 +5,31 @@ from django.shortcuts import redirect
 
 # Create your views here.
 def index(request):
+    '''
+    Renders index html template that is the shopping page
+
+    param: request: http request
+
+    return: http response with index html content
+    '''
     return render(request, "index.html")
 
 def index(request):
+    '''
+    Renders craigcv html template that is also the homepage
+
+    param: request: http request
+
+    return: http response with craigcv html content
+    '''
     return render(request, "craigcv.html")
 
 def index(request):
+    '''
+    Renders craigcvnocss html template
+
+    param: request: http request
+
+    return: http response with craigcvnocss html content
+    '''
     return render(request, "craigcvnocss.html")
